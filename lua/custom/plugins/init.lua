d16ef766898af5adcd29759fe26894a708501831
Andy Wu <uhbgvfre@gmail.com>
2026-01-15 18:29:44 +0800

    Init

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,20 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+
+-- return {}
+
+return {
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    config = function()
+      require('catppuccin').setup({
+        flavour = 'mocha', -- 可選: latte, frappe, macchiato, mocha
+      })
+      vim.cmd.colorscheme 'catppuccin'
+    end,
+  },
+}
