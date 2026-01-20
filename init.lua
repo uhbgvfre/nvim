7a72cda7f5c4617793069289ea2b930a1d05b1f5
Andy Wu <uhbgvfre@gmail.com>
2026-01-20 19:38:34 +0800

    Update cmp accept key

--- init.lua
+++ init.lua
@@ -856,6 +856,8 @@ require('lazy').setup({
         -- See :h blink-cmp-config-keymap for defining your own keymap
         preset = 'default',
 
+        ['<CR>'] = { 'select_and_accept', 'fallback' },
+
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
       },
