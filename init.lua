3cac23b455c3bf1995269891af3260f97cb476ca
Andy Wu <uhbgvfre@gmail.com>
2026-01-21 16:04:48 +0800

    Map jk to <esc>

--- init.lua
+++ init.lua
@@ -184,6 +184,7 @@ vim.o.confirm = true
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('i', 'jk', '<Esc>', { desc = 'Exit insert mode' })
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
