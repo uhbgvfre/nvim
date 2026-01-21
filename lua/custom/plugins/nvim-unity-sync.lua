f520b6cbbfb18c70d8f65f4d2be401d52f1672d5
Andy Wu <uhbgvfre@gmail.com>
2026-01-21 17:28:40 +0800

    Revert "Update cs file sync config"
    This reverts commit cd7888849830b4cf42b5dc7e4ac132b2d761572d.

--- lua/custom/plugins/nvim-unity-sync.lua
+++ lua/custom/plugins/nvim-unity-sync.lua
@@ -4,16 +4,6 @@ return {
     lazy = false,
     config = function()
       require('unity.plugin').setup()
-
-      -- Global autocommands for general file monitoring
-      local group = vim.api.nvim_create_augroup('UnitySyncGroup', { clear = true })
-      vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufNewFile', 'BufDelete' }, {
-        group = group,
-        pattern = '*.cs',
-        callback = function()
-          vim.cmd 'Usync'
-        end,
-      })
     end,
   },
 }
