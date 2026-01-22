c0b0562ccff2413abc275f5b74cff016ceb76b4a
Andy Wu <uhbgvfre@gmail.com>
2026-01-22 18:29:08 +0800

    Add theme.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,15 +6,5 @@
 -- return {}
 
 return {
-  {
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    priority = 1000,
-    config = function()
-      require('catppuccin').setup({
-        flavour = 'mocha', -- 可選: latte, frappe, macchiato, mocha
-      })
-      vim.cmd.colorscheme 'catppuccin'
-    end,
-  },
+  
 }
