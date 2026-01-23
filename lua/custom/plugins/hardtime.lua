95f3e3cdfa1727c57fe50b95a1d71dc1dec24c48
AndyWu <uhbgvfre@gmail.com>
2026-01-23 22:42:40 +0800

    Adject hardtime.nvim

--- lua/custom/plugins/hardtime.lua
+++ lua/custom/plugins/hardtime.lua
@@ -4,7 +4,13 @@ return {
     dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
     lazy = false,
     opts = {
-        max_count = 10
+        max_count = 10,
+        disabled_keys = {
+            ["<Up>"] = false,
+            ["<Down>"] = false,
+            ["<Left>"] = false,
+            ["<Right>"] = false,
+        },
     },
   },
 }
