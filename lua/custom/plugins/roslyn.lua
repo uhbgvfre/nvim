a014a3461a74c1caf93a86a2849f51dcf81731bb
Andy Wu <uhbgvfre@gmail.com>
2026-01-26 18:41:10 +0800

    Update roslyn target sln selection

--- lua/custom/plugins/roslyn.lua
+++ lua/custom/plugins/roslyn.lua
@@ -18,6 +18,14 @@ return {
         capabilities = require('blink.cmp').get_lsp_capabilities(),
       },
       exe = 'roslyn.cmd',
+      choose_target = function(targets)
+        for _, target in ipairs(targets) do
+          if target:find 'Assembly%-CSharp%.sln' then
+            return target
+          end
+        end
+        return targets[1]
+      end,
     },
   },
 }
