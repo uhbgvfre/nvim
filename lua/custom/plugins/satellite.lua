41ef7dc3dace0eb77ed655664eb52187ff5612ab
Andy Wu <uhbgvfre@gmail.com>
2026-01-27 18:29:03 +0800

    Add Satellite

---
+++ lua/custom/plugins/satellite.lua
@@ -0,0 +1,47 @@
+return {
+  {
+    "lewis6991/satellite.nvim",
+    dependencies = {
+      "lewis6991/gitsigns.nvim", -- Required for git integration
+    },
+    config = function()
+      require("satellite").setup({
+        current_only = false,
+        winblend = 50,
+        zindex = 40,
+        excluded_filetypes = {},
+        width = 2,
+        handlers = {
+          cursor = {
+            enable = true,
+            symbols = { '⎺', '⎻', '⎼', '⎽' }
+          },
+          search = {
+            enable = true,
+          },
+          diagnostic = {
+            enable = true,
+            signs = {'-', '=', '≡'},
+            min_severity = vim.diagnostic.severity.HINT,
+          },
+          gitsigns = {
+            enable = true,
+            signs = {
+              add = "│",
+              change = "│",
+              delete = "-",
+            },
+          },
+          marks = {
+            enable = true,
+            show_builtins = false,
+            key = 'm'
+          },
+          quickfix = {
+            signs = { '-', '=', '≡' },
+          }
+        },
+      })
+    end,
+  },
+}
